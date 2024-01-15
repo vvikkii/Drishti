90ef0fdf073827e1ca8393b2e7496a352323882e
vikki <1435anupsingh@gmail.com>
2024-01-15 22:19:49 +0530

    added address and new effects

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var back1=document.querySelector(".Firsthalf>#back")
 var back2=document.querySelector(".Secondhalf>#back")
 var img1=document.querySelector(".Firsthalf>#front>img")
 var img2=document.querySelector(".Secondhalf>#front>img")
-front1.addEventListener("mouseover",function(){
+front1.addEventListener("click",function(){
     img1.style.height="15vh";
     img1.style.width="15vh";
     back1.style.height="45vh";
@@ -15,11 +15,11 @@ front1.addEventListener("mouseover",function(){
     back1.style.position="relative";
     back1.style.top="-15vh";
 })
-front2.addEventListener("mouseover",function(){
+front2.addEventListener("click",function(){
     img2.style.height="15vh";
     img2.style.width="15vh";
     back2.style.height="45vh";
-    back2.style.width="25vw";
+    back2.style.width="20vw";
     back2.style.fontSize="8vh";
     back2.style.paddingTop="15vh";
     back2.style.position="relative";
@@ -36,7 +36,7 @@ back1.addEventListener("mouseout",function(){
 })
 back2.addEventListener("mouseout",function(){
     front2.style.display="flex";
-    back.style.height="0vh";
+    back2.style.height="0vh";
     back2.style.widtht="0vw";
     back2.style.fontSize="0vh";
     back2.style.paddingTop="0vh";
